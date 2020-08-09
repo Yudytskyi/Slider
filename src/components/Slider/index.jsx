f60e57db8390975cdddd916e49b1e430f09cf7f8
Yudytskyi <uan0503631989@gmail.com>
2020-08-09 21:14:10 +0300

    created a new project

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Slider.module.scss';
+
+function Slider(props) {
+  return <div className={styles.contentContainer}>Hello, Slider</div>;
+}
+
+Slider.propTypes = {};
+
+export default Slider;
